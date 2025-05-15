118cfaaa9d382c68a7d1e090aed38e7d11f8314a
Sook <sukkyu.chung@gmail.com>
2025-05-15 15:19:56 +0900

    clean up unused files & add twMerge for cn()

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,6 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-
 const experience = defineCollection({
   // Load Markdown and MDX files in the `src/content/experience/` directory.
   loader: glob({ base: "./src/content/experience", pattern: "**/*.{md,mdx}" }),
@@ -34,4 +20,4 @@ const experience = defineCollection({
   }),
 });
 
-export const collections = { blog, experience };
+export const collections = { experience };
