b12cda906bb2431afe8c080f15ce7672345816c0
Sook <sukkyu.chung@gmail.com>
2025-05-19 17:38:49 +0900

    (test) implement some React interactive logics in ImageGalleryReact

--- packages/react/src/components/ImageGallery.tsx
+++ packages/react/src/components/ImageGallery.tsx
@@ -1,28 +1,46 @@
 import { ChevronLeft, ChevronRight, Maximize } from "lucide-react";
 import { Button } from "./Button";
 import { cn } from "@website/shared/utils";
+import { useCallback, useState } from "react";
 
 export function ImageGallery({
   images,
 }: {
-  images: {
-    src: string;
-    attributes: Record<string, string | number>;
-  }[];
+  images: React.ImgHTMLAttributes<HTMLImageElement>[];
 }) {
-  return (
-    <div className="not-prose bg-black flex flex-col items-center rounded-md">
-      <div className="relative aspect-[6/4] w-full flex items-center justify-center">
-        <img src={images[0].src} {...images[0].attributes} />
+  const [activeIndex, setActiveIndex] = useState(0);
+  const activeImage = images[activeIndex];
 
-        {/* Overlay for image caption */}
+  const nextImage = useCallback(() => {
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === images.length - 1) {
+        return 0;
+      }
+      return prevIndex + 1;
+    });
+  }, [images.length]);
+
+  const prevImage = useCallback(() => {
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return images.length - 1;
+      }
+      return prevIndex - 1;
+    });
+  }, [images.length]);
+
+  return (
+    <div className="not-prose flex flex-col items-center rounded-md bg-black shadow-md shadow-slate-200">
+      <div className="relative aspect-[6/4] w-full flex items-center justify-center">
+        <img {...activeImage} />
 
         {/* Navigation buttons */}
         <Button
           variant="outline"
           size="icon"
-          className="absolute left-4 top-1/2 -translate-y-1/2"
+          className="absolute left-3 top-1/2 -translate-y-1/2"
           aria-label="Previous image"
+          onClick={prevImage}
         >
           <ChevronLeft className="h-6 w-6" />
         </Button>
@@ -30,8 +48,9 @@ export function ImageGallery({
         <Button
           variant="outline"
           size="icon"
-          className="absolute right-4 top-1/2 -translate-y-1/2"
+          className="absolute right-3 top-1/2 -translate-y-1/2"
           aria-label="Next image"
+          onClick={nextImage}
         >
           <ChevronRight className="h-6 w-6" />
         </Button>
@@ -40,20 +59,26 @@ export function ImageGallery({
         <Button
           variant="outline"
           size="icon"
-          className="absolute top-4 right-4"
+          className="absolute top-3 right-3"
           aria-label="Toggle fullscreen"
         >
           <Maximize className="h-5 w-5" />
         </Button>
       </div>
-      <div className="flex space-x-2 overflow-x-auto">
-        {images.map((image) => {
+      <div className="w-full px-3 flex space-x-2 overflow-x-auto">
+        {images.map((image, idx) => {
           return (
             <button
               key={image.src}
-              className={cn("w-28 aspect-[5/4] flex-shrink-0")}
+              className={cn(
+                "cursor-pointer w-28 aspect-[5/4] flex-shrink-0",
+                idx === activeIndex ? "brightness-100" : "brightness-50",
+              )}
+              onClick={() => {
+                setActiveIndex(idx);
+              }}
             >
-              <img src={image.src} {...image.attributes} />
+              <img className="rounded-sm" {...image} />
             </button>
           );
         })}
